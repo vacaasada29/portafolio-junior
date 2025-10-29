6da4c0f230ecaa37e0a800d9342addc5d65323c1
junior <juniorninoponce@gmail.com>
2025-10-29 12:36:54 -0500

    seccion de sobre mi donde puse mis habilidades

--- js/inicio.js
+++ js/inicio.js
@@ -23,3 +23,31 @@ function escribirEspecialidad() {
 }
 
 escribirNombre();
+
+
+
+
+/* seccion de adivinar mis habilidades */
+const habilidades = {
+  html: "HTML",
+  css: "css",
+  js: "JavaScript",
+  react: "React JS",
+  nodejs: "Node JS",
+  bootstrap: "Bootstrap",
+  sass: "SASS",
+  npm: "NPM",
+  vscode: "VS Code",
+  git: "Git",
+  github: "GitHub",
+  mysql: "MySQL"
+};
+
+const mostrarHabilidad = document.getElementById('mostrarHabilidad');
+
+document.querySelectorAll('.btn__habilidad').forEach((btnHabilidad) => {
+  btnHabilidad.addEventListener('click', () => {
+    const id = btnHabilidad.id.trim();
+    mostrarHabilidad.textContent = habilidades[id] || ""
+  });
+});
